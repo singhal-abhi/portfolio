c5a6c56cf34aeca959530f57a700651bb60d11d5
Diksha Prasad <68725958+DIKBIT@users.noreply.github.com>
2023-05-12 16:12:18 +0530

    unsuccessfull trials to use config in css :(

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import Particle from "../Particle";
 import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
-import laptopImg from "../../Assets/about.png";
+
 import Toolstack from "./Toolstack";
 import Aboutavatar from "../../Assets/about_avatar.svg";
 
