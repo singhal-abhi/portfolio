8900ee522bcc3f2e66c1c98781477b8703f57b29
singhal-abhi <abhinavsinghal256@gmail.com>
2023-05-14 21:27:27 +0530

    update

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,31 +1,17 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import {
-  SiLinux,
-  SiVisualstudiocode,
-  SiPostman,
-  SiHeroku,
-  SiVercel,
-} from "react-icons/si";
-
+import conf from "../../config.json";
+import IconSearch from './IconSearch'
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiLinux />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVisualstudiocode />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiPostman />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiVercel />
-      </Col>
-      <Col xs={4} md={2} className="tech-icons">
-        <SiHeroku />
-      </Col>
+      {
+        conf.tools.map((icon) =>
+          <Col xs={4} md={2} className="tech-icons">
+            {IconSearch(icon)}
+          </Col>
+        )
+      }
     </Row>
   );
 }
